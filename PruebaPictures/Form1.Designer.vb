2a84ff4bd6df9ef411597e635a4bb22e6eaf117b
CrisHendricks (Cristina Sicre F.) <cristina.sicre.f@hotmail.com>
2014-09-24 21:28:45 +0200

    Juego de lógica

---
+++ PruebaPictures/Form1.Designer.vb
@@ -0,0 +1,407 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.cb1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cbIa2 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.pct15 = New System.Windows.Forms.PictureBox()
+        Me.pct14 = New System.Windows.Forms.PictureBox()
+        Me.pct13 = New System.Windows.Forms.PictureBox()
+        Me.pct12 = New System.Windows.Forms.PictureBox()
+        Me.pct11 = New System.Windows.Forms.PictureBox()
+        Me.pct10 = New System.Windows.Forms.PictureBox()
+        Me.pct9 = New System.Windows.Forms.PictureBox()
+        Me.pct8 = New System.Windows.Forms.PictureBox()
+        Me.pct7 = New System.Windows.Forms.PictureBox()
+        Me.pct6 = New System.Windows.Forms.PictureBox()
+        Me.pct5 = New System.Windows.Forms.PictureBox()
+        Me.pct4 = New System.Windows.Forms.PictureBox()
+        Me.pct3 = New System.Windows.Forms.PictureBox()
+        Me.pct2 = New System.Windows.Forms.PictureBox()
+        Me.pct1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pct1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn1.Font = New System.Drawing.Font("Impact", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(336, 202)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn3.Font = New System.Drawing.Font("Impact", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(498, 202)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(75, 23)
+        Me.btn3.TabIndex = 2
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.LavenderBlush
+        Me.btn2.Font = New System.Drawing.Font("Impact", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(417, 202)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(75, 23)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Impact", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(42, 207)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(94, 26)
+        Me.lbl1.TabIndex = 21
+        Me.lbl1.Text = "Jugador 1"
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Font = New System.Drawing.Font("Impact", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl2.Location = New System.Drawing.Point(765, 206)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(97, 26)
+        Me.lbl2.TabIndex = 22
+        Me.lbl2.Text = "Jugador 2"
+        '
+        'cb1
+        '
+        Me.cb1.AutoSize = True
+        Me.cb1.Location = New System.Drawing.Point(768, 237)
+        Me.cb1.Name = "cb1"
+        Me.cb1.Size = New System.Drawing.Size(36, 17)
+        Me.cb1.TabIndex = 23
+        Me.cb1.Text = "IA"
+        Me.cb1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("BadaBoom BB", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(226, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(441, 102)
+        Me.Label1.TabIndex = 24
+        Me.Label1.Text = "15 MONEDAS"
+        '
+        'cbIa2
+        '
+        Me.cbIa2.AutoSize = True
+        Me.cbIa2.Location = New System.Drawing.Point(813, 237)
+        Me.cbIa2.Name = "cbIa2"
+        Me.cbIa2.Size = New System.Drawing.Size(49, 17)
+        Me.cbIa2.TabIndex = 25
+        Me.cbIa2.Text = "dificil"
+        Me.cbIa2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MineCrafter 3", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(728, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(148, 18)
+        Me.Label2.TabIndex = 26
+        Me.Label2.Text = "by CrisHendricks"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.PruebaPictures.My.Resources.Resources.gnomo2
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox2.Location = New System.Drawing.Point(710, 173)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(52, 81)
+        Me.PictureBox2.TabIndex = 28
+        Me.PictureBox2.TabStop = False
+        '
+        'pct15
+        '
+        Me.pct15.BackColor = System.Drawing.Color.Turquoise
+        Me.pct15.Location = New System.Drawing.Point(823, 116)
+        Me.pct15.Name = "pct15"
+        Me.pct15.Size = New System.Drawing.Size(52, 51)
+        Me.pct15.TabIndex = 20
+        Me.pct15.TabStop = False
+        '
+        'pct14
+        '
+        Me.pct14.BackColor = System.Drawing.Color.Turquoise
+        Me.pct14.Location = New System.Drawing.Point(765, 116)
+        Me.pct14.Name = "pct14"
+        Me.pct14.Size = New System.Drawing.Size(52, 51)
+        Me.pct14.TabIndex = 19
+        Me.pct14.TabStop = False
+        '
+        'pct13
+        '
+        Me.pct13.BackColor = System.Drawing.Color.Turquoise
+        Me.pct13.Location = New System.Drawing.Point(707, 116)
+        Me.pct13.Name = "pct13"
+        Me.pct13.Size = New System.Drawing.Size(52, 51)
+        Me.pct13.TabIndex = 18
+        Me.pct13.TabStop = False
+        '
+        'pct12
+        '
+        Me.pct12.BackColor = System.Drawing.Color.Turquoise
+        Me.pct12.Location = New System.Drawing.Point(649, 116)
+        Me.pct12.Name = "pct12"
+        Me.pct12.Size = New System.Drawing.Size(52, 51)
+        Me.pct12.TabIndex = 14
+        Me.pct12.TabStop = False
+        '
+        'pct11
+        '
+        Me.pct11.BackColor = System.Drawing.Color.Turquoise
+        Me.pct11.Location = New System.Drawing.Point(591, 116)
+        Me.pct11.Name = "pct11"
+        Me.pct11.Size = New System.Drawing.Size(52, 51)
+        Me.pct11.TabIndex = 13
+        Me.pct11.TabStop = False
+        '
+        'pct10
+        '
+        Me.pct10.BackColor = System.Drawing.Color.Turquoise
+        Me.pct10.Location = New System.Drawing.Point(533, 116)
+        Me.pct10.Name = "pct10"
+        Me.pct10.Size = New System.Drawing.Size(52, 51)
+        Me.pct10.TabIndex = 12
+        Me.pct10.TabStop = False
+        '
+        'pct9
+        '
+        Me.pct9.BackColor = System.Drawing.Color.Turquoise
+        Me.pct9.Location = New System.Drawing.Point(475, 116)
+        Me.pct9.Name = "pct9"
+        Me.pct9.Size = New System.Drawing.Size(52, 51)
+        Me.pct9.TabIndex = 11
+        Me.pct9.TabStop = False
+        '
+        'pct8
+        '
+        Me.pct8.BackColor = System.Drawing.Color.Turquoise
+        Me.pct8.Location = New System.Drawing.Point(417, 116)
+        Me.pct8.Name = "pct8"
+        Me.pct8.Size = New System.Drawing.Size(52, 51)
+        Me.pct8.TabIndex = 10
+        Me.pct8.TabStop = False
+        '
+        'pct7
+        '
+        Me.pct7.BackColor = System.Drawing.Color.Turquoise
+        Me.pct7.Location = New System.Drawing.Point(359, 116)
+        Me.pct7.Name = "pct7"
+        Me.pct7.Size = New System.Drawing.Size(52, 51)
+        Me.pct7.TabIndex = 9
+        Me.pct7.TabStop = False
+        '
+        'pct6
+        '
+        Me.pct6.BackColor = System.Drawing.Color.Turquoise
+        Me.pct6.Location = New System.Drawing.Point(301, 116)
+        Me.pct6.Name = "pct6"
+        Me.pct6.Size = New System.Drawing.Size(52, 51)
+        Me.pct6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pct6.TabIndex = 8
+        Me.pct6.TabStop = False
+        '
+        'pct5
+        '
+        Me.pct5.BackColor = System.Drawing.Color.Turquoise
+        Me.pct5.Location = New System.Drawing.Point(243, 116)
+        Me.pct5.Name = "pct5"
+        Me.pct5.Size = New System.Drawing.Size(52, 51)
+        Me.pct5.TabIndex = 7
+        Me.pct5.TabStop = False
+        '
+        'pct4
+        '
+        Me.pct4.BackColor = System.Drawing.Color.Turquoise
+        Me.pct4.Location = New System.Drawing.Point(185, 116)
+        Me.pct4.Name = "pct4"
+        Me.pct4.Size = New System.Drawing.Size(52, 51)
+        Me.pct4.TabIndex = 6
+        Me.pct4.TabStop = False
+        '
+        'pct3
+        '
+        Me.pct3.BackColor = System.Drawing.Color.Turquoise
+        Me.pct3.Location = New System.Drawing.Point(127, 116)
+        Me.pct3.Name = "pct3"
+        Me.pct3.Size = New System.Drawing.Size(52, 51)
+        Me.pct3.TabIndex = 5
+        Me.pct3.TabStop = False
+        '
+        'pct2
+        '
+        Me.pct2.BackColor = System.Drawing.Color.Turquoise
+        Me.pct2.Location = New System.Drawing.Point(69, 116)
+        Me.pct2.Name = "pct2"
+        Me.pct2.Size = New System.Drawing.Size(52, 51)
+        Me.pct2.TabIndex = 4
+        Me.pct2.TabStop = False
+        '
+        'pct1
+        '
+        Me.pct1.BackColor = System.Drawing.Color.Turquoise
+        Me.pct1.Location = New System.Drawing.Point(11, 116)
+        Me.pct1.Name = "pct1"
+        Me.pct1.Size = New System.Drawing.Size(52, 51)
+        Me.pct1.TabIndex = 0
+        Me.pct1.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.PruebaPictures.My.Resources.Resources.copy_of_gnomo_1_1_
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(142, 173)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(52, 81)
+        Me.PictureBox1.TabIndex = 29
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Turquoise
+        Me.ClientSize = New System.Drawing.Size(888, 254)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cbIa2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cb1)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.pct15)
+        Me.Controls.Add(Me.pct14)
+        Me.Controls.Add(Me.pct13)
+        Me.Controls.Add(Me.pct12)
+        Me.Controls.Add(Me.pct11)
+        Me.Controls.Add(Me.pct10)
+        Me.Controls.Add(Me.pct9)
+        Me.Controls.Add(Me.pct8)
+        Me.Controls.Add(Me.pct7)
+        Me.Controls.Add(Me.pct6)
+        Me.Controls.Add(Me.pct5)
+        Me.Controls.Add(Me.pct4)
+        Me.Controls.Add(Me.pct3)
+        Me.Controls.Add(Me.pct2)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.pct1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "15 Monedas"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pct1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pct1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents pct2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct3 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct6 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct5 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct4 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct12 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct11 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct10 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct9 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct8 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct7 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct15 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct14 As System.Windows.Forms.PictureBox
+    Friend WithEvents pct13 As System.Windows.Forms.PictureBox
+    Friend WithEvents lbl1 As System.Windows.Forms.Label
+    Friend WithEvents lbl2 As System.Windows.Forms.Label
+    Friend WithEvents cb1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cbIa2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ PruebaPictures/Form1.vb
@@ -0,0 +1,193 @@
+﻿Public Class Form1
+
+    Dim numMonedas As Integer = 15
+    Dim turno As Integer = 1
+    Dim arrayImagenes() As PictureBox
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MsgBox(" Reglas del Juego " & vbCrLf & " -----------------------------------------------------------------------------" & vbCrLf & "Cada jugador puede durante su turno coger una, dos o tres monedas pero no más. Pierde el jugador que se ve obligado a recoger la última moneda sobre la mesa. También puedes jugar contra el ordenador en dos niveles de dificultad.", , "Quince Monedas")
+        dibujar()
+
+    End Sub
+
+    Public Sub dibujar()
+        lbl1.ForeColor = Color.Blue
+
+        turno = 1
+        lbl1.ForeColor = Color.Blue
+        lbl2.ForeColor = Color.Black
+
+        numMonedas = 15
+
+        arrayImagenes = {pct1, pct2, pct3, pct4, pct5, pct6, pct7, pct8, pct9, pct10, pct11, pct12, pct13, pct14, pct15}
+      
+        For i As Integer = 0 To arrayImagenes.Length - 1
+
+            arrayImagenes(i).Image = PruebaPictures.My.Resources.euro1
+            arrayImagenes(i).SizeMode = PictureBoxSizeMode.Zoom
+        Next
+
+        btn1.Enabled = True
+        btn2.Enabled = True
+        btn3.Enabled = True
+    End Sub
+
+
+    Public Sub jugar()
+        btn1.Enabled() = True
+        btn2.Enabled() = True
+        btn3.Enabled() = True
+    End Sub
+
+
+    Private Sub btn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn3.Click
+
+        Dim monedasARestar As Integer = 3
+
+        For i As Integer = 0 To monedasARestar - 1
+            arrayImagenes(numMonedas - 1 - i).Image = PruebaPictures.My.Resources.euro2
+        Next
+
+        numMonedas -= monedasARestar
+
+        compRestantes()
+        ComprobarGanador()
+
+    End Sub
+    Private Sub btn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn2.Click
+
+        Dim monedasARestar As Integer = 2
+
+        For i As Integer = 0 To monedasARestar - 1
+            arrayImagenes(numMonedas - 1 - i).Image = PruebaPictures.My.Resources.euro2
+        Next
+        numMonedas -= monedasARestar
+
+        compRestantes()
+        ComprobarGanador()
+
+    End Sub
+    Private Sub btn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn1.Click
+
+
+        arrayImagenes(numMonedas - 1).Image = PruebaPictures.My.Resources.euro2
+        numMonedas -= 1
+
+        compRestantes()
+        ComprobarGanador()
+        
+
+        
+
+    End Sub
+
+    Public Sub ComprobarGanador()
+        If compGanar() Then
+
+            MsgBox("Gana el jugador " & turno)
+
+            dibujar()
+        Else
+            cambiarTurno()
+        End If
+    End Sub
+
+    Public Function compGanar()
+        If numMonedas = 1 Then
+            Return True
+        Else
+            Return False
+
+        End If
+
+    End Function
+
+    Public Sub compRestantes()
+        If numMonedas = 3 Then
+
+            btn3.Enabled = False
+
+
+        ElseIf numMonedas = 2 Then
+
+            btn2.Enabled = False
+            btn3.Enabled = False
+
+        End If
+    End Sub
+    Public Sub cambiarTurno()
+        If turno = 1 Then
+            turno = 2
+            lbl2.ForeColor = Color.Blue
+            lbl1.ForeColor = Color.Black
+            If cb1.Checked = True Then
+                If cbIa2.Checked Then
+                    iaDificil()
+                Else
+                    ia()
+                End If
+
+            End If
+        Else
+            turno = 1
+            lbl1.ForeColor = Color.Blue
+            lbl2.ForeColor = Color.Black
+        End If
+    End Sub
+
+
+    Public Sub ia()
+        Randomize()
+        Dim aleatorio As Integer = Math.Truncate((Rnd() * 3) + 1)
+
+        Select Case aleatorio
+            Case 1
+
+                If Not btn1.Enabled Then
+                    ia()
+                Else
+                    btn1_Click(Nothing, Nothing)
+                End If
+            Case 2
+
+                If Not btn2.Enabled Then
+                    ia()
+                Else
+                    btn2_Click(Nothing, Nothing)
+                End If
+            Case 3
+                If Not btn3.Enabled Then
+                    ia()
+                Else
+                    btn3_Click(Nothing, Nothing)
+                End If
+        End Select
+
+    End Sub
+
+    Public Sub iaDificil()
+        If numMonedas = 6 Then
+
+        End If
+        Select Case numMonedas
+            Case 2
+                btn1_Click(Nothing, Nothing)
+            Case 3
+                btn2_Click(Nothing, Nothing)
+            Case 4
+                btn3_Click(Nothing, Nothing)
+            Case 5
+                btn1_Click(Nothing, Nothing)
+            Case 6
+                btn1_Click(Nothing, Nothing)
+            Case 7
+                btn2_Click(Nothing, Nothing)
+            Case 8
+                btn3_Click(Nothing, Nothing)
+            Case Else
+                ia()
+
+        End Select
+    End Sub
+
+End Class
